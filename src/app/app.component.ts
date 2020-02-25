5e703e2b8d14078a2a6e497edcf77e42d11d08b3
ckocaogullar15 <ckocaogullar15@ku.edu.tr>
2020-02-25 21:49:34 +0300

    Added basic text sentiment analysis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'emotion-sonification';
+  title = "emotion-sonification";
+  message = "";
+  constructor(private http: HttpClient) {}
+
+  submit() {
+    this.http
+      .post<any>(
+        "https://zg6r2c4n0m.execute-api.us-east-1.amazonaws.com/Default/analyzesentiment",
+        this.message,
+        {
+          headers: new HttpHeaders({
+            "Content-Type": "text/plain",
+            "x-api-key": "Jb0JTIbC2y1WrNpFdIKqx702XULhNPYZ37eU9IMJ"
+          })
+        }
+      )
+      .subscribe(result => console.log(result));
+  }
 }
