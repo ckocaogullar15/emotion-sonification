cf98b4f93cf02ba84d4d7e089934064fab0be2ee
ckocaogullar15 <ckocaogullar15@ku.edu.tr>
2020-03-01 16:47:31 +0300

    Basic video recorder working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,11 @@
-import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { HttpClientModule } from "@angular/common/http";
-import { FormsModule } from "@angular/forms";
+import { VideoJSRecordComponent } from "./videojs.record.component";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule],
+  declarations: [VideoJSRecordComponent],
+  bootstrap: [VideoJSRecordComponent]
 })
 export class AppModule {}
